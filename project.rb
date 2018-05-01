a0027288361dc82529c20638ee66392c29f8e9a5
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2018-05-01 13:30:30 +0200

    added documentation

--- project.rb
+++ project.rb
@@ -1,17 +1,21 @@
-#!/usr/bin/ruby
-
 require "json"
 
 class Project
+
+  # Initializes this object
+  # Params:
+  # +json_path+:: a tring that hold the path to a specific configuration file in json format
   def initialize(json_path)
     @data = JSON.parse File.read json_path
     @name = @data["project_name"]
   end
 
+  # Returns the data JSON object
   def data
     @data
   end
 
+  # Returns the project's name of this object
   def name
     @name
   end
